9ecfe4fce992d3ce2489ec7bc2676717f90bf7b8
hawar <iaramkhzri@gmail.com>
2025-08-29 16:29:06 +0330

    hello-world added

---
+++ README.md
@@ -0,0 +1,27 @@
+# React Native Learning!
+This repo contains some small projects that I have developed in my journey of learning React Native.
+
+## Using
+```bash
+git clone <project_url>
+
+cd react-native-projects
+
+cd <a project folder>
+
+npm install
+
+# run a development server
+npm run start
+# or
+npx expo start
+
+#‌ build using eas
+npm install -f eas-cli
+
+eas login
+
+eas build:configure
+
+eas build
+```
---
+++ hello-world/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>سلام مهتاب!</Text>
+      <Text>😃ممنون بابت پنج شنبه زیبا</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
